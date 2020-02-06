ae7a248e00fe77bdaaaebfe76eb5bc4c4b600df3
jepoy3134 <asdggasdwpasd@gmail.com>
2020-02-06 20:01:37 +0800

    commit 6

---
+++ js/Script.js
@@ -0,0 +1,13 @@
+
+function showmessage(){
+	alert("Hi Lets learn javascript");
+
+	for(var i=5; i>0; i--){
+	alert("Variable I is now: "+ i);
+	}
+}
+function getdouble(){
+	
+	var totel = document.getElementsByTagName("p");
+	alert(totel.length);
+}
---
+++ js/link.js
@@ -0,0 +1,5 @@
+function myFunction() {
+	var user = document.getElementById("Username").value;
+  	var pass = document.getElementById("Password").value;
+	alert (pass);
+}
